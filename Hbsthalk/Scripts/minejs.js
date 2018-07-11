53db260679160eb190e7790090612b546d1e097c
Heshamovic <mohamed.97198@gmail.com>
2018-07-11 02:48:17 +0200

    starting filter panel

--- Hbsthalk/Scripts/minejs.js
+++ Hbsthalk/Scripts/minejs.js
@@ -107,3 +107,23 @@ var signIn = function () {
 }
 /*END*/
 //Home tutorial js
+
+    /*subFilter*/
+$(function () {
+    $(".tree span").append($("<i class='fa fa-plus fa-fw'></i>"));
+    $(".tree>ul ul").hide();
+    $(".tree span").click(function (event) {
+        var $this = $(this);
+        var $ul = $this.parent().children("ul");
+
+        if ($ul.length > 0) {
+            if ($ul.is(":visible")) {
+                $ul.slideUp();
+                $this.children("i").removeClass("fa-minus").addClass("fa-plus");
+            } else {
+                $ul.slideDown();
+                $this.children("i").removeClass("fa-plus").addClass("fa-minus");
+            }
+        }
+    });
+});
