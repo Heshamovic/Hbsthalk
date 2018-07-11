7121d38c05c13d9c07d0be945b107b9d533785a5
Heshamovic <mohamed.97198@gmail.com>
2018-07-11 19:14:52 +0200

    updating filter panel

--- Hbsthalk/Scripts/minejs.js
+++ Hbsthalk/Scripts/minejs.js
@@ -3,15 +3,11 @@ $(window).on('load', function () {
     $(".overlay").fadeOut(1000, function () {
         $("body").css("overflow", "auto");
     });
-
-
     if ($(window).scrollTop()) {
         $('nav').css("background-color", "#881125");
         $('.navbar').css("height", "90px");
         $('.nav-link').hover(function () {
             $('.nav-link:hover').css("color", "black");
-
-
         });
     }
     else {
@@ -127,3 +123,126 @@ $(function () {
         }
     });
 });
+/*reg*/
+$(".regBtn").click(function () {
+    $('body').css("overflow", "hidden");
+    $('.register').addClass("reg-active");
+});
+
+$('.register').click(function () {
+    /*$(this).css("cursor","auto");*/
+    $('body').css("overflow", "auto");
+    $(this).removeClass("reg-active");
+});
+$('.register .reg-inner').click(function (e) {
+    e.stopPropagation();
+});
+/*end*//*start counter*/
+// Set the date we're counting down to
+var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("d").innerHTML = days;
+    document.getElementById("h").innerHTML = hours;
+    document.getElementById("m").innerHTML = minutes;
+    document.getElementById("s").innerHTML = seconds;
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+/*END*/
+
+
+
+
+
+
+
+
+/*Start Blogs*/
+
+$("#myCarousel").on("slide.bs.carousel", function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction === "left") {
+                $(".carousel-item")
+                    .eq(i)
+                    .appendTo(".carousel-inner");
+            } else {
+                $(".carousel-item")
+                    .eq(0)
+                    .appendTo($(this).find(".carousel-inner"));
+            }
+        }
+    }
+});
+
+
+
+/*END*/
+/*start blogs*/
+$(function () {
+    $('.material-card > .mc-btn-action').click(function () {
+        var card = $(this).parent('.material-card');
+        var icon = $(this).children('i');
+        icon.addClass('fa-spin-fast');
+
+        if (card.hasClass('mc-active')) {
+            card.removeClass('mc-active');
+
+            window.setTimeout(function () {
+                icon
+                    .removeClass('fa-arrow-left')
+                    .removeClass('fa-spin-fast')
+                    .addClass('fa-bars');
+
+            }, 800);
+        } else {
+            card.addClass('mc-active');
+
+            window.setTimeout(function () {
+                icon
+                    .removeClass('fa-bars')
+                    .removeClass('fa-spin-fast')
+                    .addClass('fa-arrow-left');
+
+            }, 800);
+        }
+    });
+});
+
+/*END*/
+
+
+/*Questions pop*/
+$(".popper").click(function () {
+    $('body').css("overflow", "hidden");
+    $('.Qpop').addClass("Qpop-active");
+});
+ /*End Questions pop*/
